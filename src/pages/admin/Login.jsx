ba346c7a94c49d7bcec366e74ca39b5184cbbed5
jmagwili <joshuamagwili@gmail.com>
2023-11-06 08:49:05 +0800

    updated login page

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -2,14 +2,41 @@ import '../../styles/admin/login.css'
 
 export const Login = () => {
     return(
-        <div className="login-page">
-            <div className="login-wrapper">
-                <form>
-                    <input type="text" placeholder="Username" /><br />
-                    <input type="password" placeholder="password"/> <br />
-                    <input type="submit" />
-                </form>
-            </div>
-        </div>
+        <>
+            <body className="login">
+                
+                <div class="container">
+                    <div class="login-container-wrapper clearfix">
+                        <div class="logo">
+                            <i class="fa fa-sign-in"></i>
+                        </div>
+                        <div class="welcome"><strong>Welcome,</strong> please login</div>
+
+                        <form class="form-horizontal login-form">
+                            <div class="form-group relative">
+                                <input id="login_username" class="form-control input-lg" type="email" placeholder="Username" required />
+                                <i class="fa fa-user"></i>
+                            </div>
+                            <div class="form-group relative password">
+                                <input id="login_password" class="form-control input-lg" type="password" placeholder="Password" required />
+                                <i class="fa fa-lock"></i>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success btn-lg btn-block">Login</button>
+                            </div>
+                            <div class="checkbox pull-left">
+                                <label><input type="checkbox" /> Remember</label>
+                            </div>
+                            <div class="checkbox pull-right">
+                                <label> <a class="forget" href="" title="forget">Forgot your password</a> </label>
+                            </div>
+                        </form>
+                    </div>
+            
+                </div>
+            </body>
+            
+            
+        </>
     )
 }
