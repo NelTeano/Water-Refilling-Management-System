06209064d4a8876976ea5e4f9c3334c302c17464
jmagwili <joshuamagwili@gmail.com>
2023-11-06 22:33:50 +0800

    uninstalled react-auth-kit

--- server/routes/admin/Login.js
+++ server/routes/admin/Login.js
@@ -1,10 +1,12 @@
 import express from "express"
-import {userModel} from "../../models/users"
+import userModel from '../../models/users.js'
 
 const LoginRoute = express.Router()
 
-LoginRoute.post('/',(req,res)=>{
-    res.send("test")
+LoginRoute.post('/',async (req,res)=>{
+    const {username,password} = req.body
+    res.send(`username: ${username} password: ${password}`)
+    console.log(username,password)
 })
 
 export default LoginRoute
--- src/main.jsx
+++ src/main.jsx
@@ -8,17 +8,29 @@ import { Auth0Provider } from '@auth0/auth0-react';
 // REACT ROUTER PROVIDER
 import { BrowserRouter } from 'react-router-dom'
 
+// REACT-AUTH-KIT PROVIDER
+// import AuthProvider from 'react-auth-kit'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-  domain={import.meta.env.VITE_AUTH0_DOMAIN}
-  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-  authorizationParams={{
-    redirect_uri: window.location.origin
-  }}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Auth0Provider>,
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    {/* <AuthProvider 
+      authType = {'cookie'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    > */}
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    {/* </AuthProvider> */}
+  </Auth0Provider>
 )
