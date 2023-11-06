363ec82030e73363c6c1e597ecbe52acd4acca12
jmagwili <joshuamagwili@gmail.com>
2023-11-06 10:26:53 +0800

    updated admin login style

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -3,23 +3,20 @@ import '../../styles/admin/login.css'
 export const Login = () => {
     return(
         <>
-            <body className="login">
+            <div className="login">
                 
                 <div class="container">
                     <div class="login-container-wrapper clearfix">
-                        <div class="logo">
-                            <i class="fa fa-sign-in"></i>
-                        </div>
-                        <div class="welcome"><strong>Welcome,</strong> please login</div>
+                        <div class="welcome">ADMIN LOGIN</div>
 
                         <form class="form-horizontal login-form">
                             <div class="form-group relative">
                                 <input id="login_username" class="form-control input-lg" type="email" placeholder="Username" required />
-                                <i class="fa fa-user"></i>
+                             
                             </div>
                             <div class="form-group relative password">
                                 <input id="login_password" class="form-control input-lg" type="password" placeholder="Password" required />
-                                <i class="fa fa-lock"></i>
+                              
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-success btn-lg btn-block">Login</button>
@@ -34,7 +31,7 @@ export const Login = () => {
                     </div>
             
                 </div>
-            </body>
+            </div>
             
             
         </>
