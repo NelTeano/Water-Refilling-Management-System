d1fa334f0908c9e528fcfd5255c01173ac696a84
jmagwili <joshuamagwili@gmail.com>
2023-11-25 23:36:19 +0800

    code clean up

--- server/routes/admin/PassportConfig.js
+++ server/routes/admin/PassportConfig.js
@@ -4,7 +4,16 @@ import userModel from '../../models/users.js';
 import bcrypt from 'bcrypt'
 
 export default function(passport) {
+    
+    passport.serializeUser((user,cb)=>{
+        cb(null, user.id)
+    })
 
+    passport.deserializeUser((id,cb)=>{
+        console.log('im called')
+        userModel.findOne({_id:id},(err,data)=>cb(err,data))
+    })
+    
     passport.use(
         new LocalStrategy((username,password,done)=>{
             userModel.findOne({username:username})
@@ -18,13 +27,4 @@ export default function(passport) {
         })
     )
 
-    passport.serializeUser((user,cb)=>{
-        cb(null, user.id)
-    })
-
-    passport.deserializeUser((id,cb)=>{
-        userModel.findOne({_id:id})
-            .then((data) => cb(null,data))
-            .catch((err) => cb(err, data))
-    })
 }
--- server/server.js
+++ server/server.js
@@ -16,15 +16,11 @@ import cookieParser from 'cookie-parser'
 // DATABASE CONNECTION
 import { initDatabase } from './database.js'
 
-//AUTH CHECKER MIDDLEWARE
-import { EnsureAuthentication } from "./routes/admin/EnsureAuthenticated.js";
-
 // CLIENT ROUTES 
 import UserRoutes from './routes/client/UserRoutes.js'
 
 //ADMIN ROUTES
-import LoginRoute from "./routes/admin/Login.js";
-//import passportInit from './routes/admin/PassportConfig.js';
+import PassportConfig from './routes/admin/PassportConfig.js';
 
 const app = express();
 dotenv.config();      // ACCESS .ENV 
@@ -32,18 +28,6 @@ initDatabase();
 
 const PORT = 5174;
 console.log("app is running");
-
-//const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-
-// const store = new MongoDBStore({
-//   uri: process.env.VITE_DATABASE_URI,
-//   expires: 1000 * 60 * 60 * 2, // 2 hours expiration time for sessions (in milliseconds)
-// });
-
-// store.on('error', function (error) {
-//   console.error('MongoDBStore error:', error);
-// });
 
 //MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
@@ -53,27 +37,15 @@ app.use(cors({
     origin: ['http://localhost:5173','http://localhost:5173'],  // THE HTTP(ORIGIN) THAT WILL ALLOW TO ACCESS THE ROUTES
     credentials: true,
   }));
-
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
 }));
-
 app.use(cookieParser('keyboard cat'))
 app.use(passport.initialize())
 app.use(passport.session())
-
-import PassportConfig from './routes/admin/PassportConfig.js';
 PassportConfig(passport);
-
-
-
-//PASSPORT INIT
-//app.use(passport.authenticate('session'));
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 
 // ACTIVATE SERVER PORT 
@@ -82,23 +54,22 @@ app.listen(PORT, function () {
 });
 
 app.use('/api', UserRoutes); // FOR TESTING ROUTE WORKS
-//app.use('/login',LoginRoute)
 
-app.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-      if (err) {
-          return res.status(500).json({ message: 'An error occurred' });
-      }
-      if (!user) {
-          return res.status(401).json({ message: 'Incorrect username or password' });
-      }
-      req.logIn(user, (err) => {
-          if (err) {
-              return res.status(500).json({ message: 'Login failed' });
-          }
-          return res.status(200).json({ message: 'Login successful', user: req.user });
-      });
-  })(req, res, next);
-});
 
+app.post('/login', passport.authenticate('local'),(req, res, next) => {
+  return res.status(200).json({ message: 'Login successful', user: req.user });
+})
+
+app.get('/checker',(req,res)=> {
+  res.send(req.isAuthenticated())
+})
+
+const isAuth = (req,res,next) => {
+  if(req.user) next()
+  res.send(req.user)
+}
+
+app.get('/',isAuth,(req,res)=>{
+  res.send('ok')
+})
 
