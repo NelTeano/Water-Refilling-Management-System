874617481e1b4b400d561e803b968a8021c84661
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-11-27 20:04:24 +0800

    added material ui dependencies

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,16 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import { Box } from "@mui/material"
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
 const DashboardPage = () => {
     const {user, isAuthenticated} = useAuth0()
 
     return (
-        <div>
+        <Box sx={{ background: 'red' }}>
             {
                 isAuthenticated ? 
                 <p>Welcome {user.given_name}!</p> : 
                 <p>You are not logged in</p>
             }
-        </div>
+            <AccessAlarm sx={{ color: 'white' }} />
+            <ThreeDRotation />
+        </Box>
     )
 }
 
