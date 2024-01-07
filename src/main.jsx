503664c21d6d3aaa2bd106f0d95e05c7890148fa
Eliz Marie Manalo <tech.ezly@gmail.com>
2024-01-07 21:29:15 +0800

    Created Theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ThemeProvider from './theme/index.jsx';
 
 // AUTH0 PROVIDER
 import { Auth0Provider } from '@auth0/auth0-react';
@@ -21,9 +22,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     }}
   >
       <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+          <ThemeProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+          </ThemeProvider>
       </React.StrictMode>
   </Auth0Provider>
 )
