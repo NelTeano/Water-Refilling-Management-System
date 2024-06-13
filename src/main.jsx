3add8f36e8a0b2c71ff70eb0235741fa3f695c55
JONEL <jonelteano29@gmail.com>
2024-06-13 19:09:49 +0800

    putting back the windows origin

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
   >
       <React.StrictMode>
           <ThemeProvider>
