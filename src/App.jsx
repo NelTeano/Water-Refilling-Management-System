ae723f1b3ab479d987d7837c8eb5ebb8c47cd26e
JONEL <jonelteano29@gmail.com>
2024-06-13 20:25:36 +0800

    fix the header rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 // PAGES
-import HeaderAuth from './components/Auth'
-import Home from './pages/HomePage/Home'
+import HeaderAuth from './components/Auth';
+import Home from './pages/HomePage/Home';
 import DashboardPage from './pages/DashboardPage';
 import LocationPage from './pages/LocationPage';
 import OrderPage from './pages/OrderPage';
@@ -12,65 +13,61 @@ import OrderStatusPage from './pages/OrderStatusPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 
 // STYLES
-import './App.css'
+import './App.css';
 
 function App() {
+  const location = useLocation();
+  const [isAdminRoute, setIsAdminRoute] = useState(false);
 
-    const routes = [
-      {
-        pathname: "/",
-        element: Home
-      },
-      {
-        pathname: "/client-dashboard",
-        element: DashboardPage
-        // children: [
-        //   {
-        //     pathname: "/location",
-        //     element: LocationPage
-        //   },
-        // ]
-      },
-      {
-        pathname: "/client-dashboard/location",
-        element: LocationPage
-      },
-      {
-        pathname: "/client-dashboard/order-status/:status",
-        element: OrderStatusPage
-      },
-      {
-        pathname: "/client-dashboard/order/:orderType",
-        element: OrderPage
-      },
-      {
-        pathname: "/receipt/:orderSummary",
-        element: ReceiptPage
-      },
-      {
-        pathname: "/register",
-        element: RegisterPage
-      },
-      {
-        pathname: "/history",
-        element: OrderHistoryPage
-      },
-      
-    ];
+  useEffect(() => {
+    setIsAdminRoute(location.pathname.startsWith('/client-dashboard'));
+  }, [location]);
 
-    const isAdminRoute = location.pathname.startsWith('/client-dashboard');
-    // const isProtectedRoute = location.pathname.startsWith('/admin/dashboard');
+  const routes = [
+    {
+      pathname: "/",
+      element: Home,
+    },
+    {
+      pathname: "/client-dashboard",
+      element: DashboardPage,
+    },
+    {
+      pathname: "/client-dashboard/location",
+      element: LocationPage,
+    },
+    {
+      pathname: "/client-dashboard/order-status/:status",
+      element: OrderStatusPage,
+    },
+    {
+      pathname: "/client-dashboard/order/:orderType",
+      element: OrderPage,
+    },
+    {
+      pathname: "/receipt/:orderSummary",
+      element: ReceiptPage,
+    },
+    {
+      pathname: "/register",
+      element: RegisterPage,
+    },
+    {
+      pathname: "/history",
+      element: OrderHistoryPage,
+    },
+  ];
 
-    return(
-      <>
-        {isAdminRoute && <HeaderAuth />}
-          <Routes>
-          {routes.map((route, index)=> (
-            <Route key={index} path={route.pathname} element={<route.element />} />
-          ))}
-        </Routes>
-      </>
-    )
+  return (
+    <>
+      {isAdminRoute && <HeaderAuth />}
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.pathname} element={<route.element />} />
+        ))}
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
